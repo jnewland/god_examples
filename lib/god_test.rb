301fb87cea682c982445adc3e4b730d07bde0ccf
Jesse Newland <jnewland@gmail.com>
2008-07-08 16:45:35 -0400

    some documentation

--- lib/god_test.rb
+++ lib/god_test.rb
@@ -1,3 +1,11 @@
+#A simple ruby class that loops and prints the current time. It will
+#optionally leak memory or crash, depending on the keys passed into the
+#++options++ hash; ++:delay++ controls how often the loop runs (defaults to 30
+#seconds), ++:leakiness++ controls how much memory to leak per loop
+#(if leakiness > 0, a new instance variable is created every loop that is 
+#leakiness * the size of this file, and ++:crashiness++ determines how many
+#times to loop (on average) before crashing.
+
 class GodTest
 
   def initialize(options = {})
@@ -19,7 +27,7 @@ class GodTest
   def crash
     raise "Crash!" if rand(@crashiness) == 1 || @crashiness == 1
   end
-  
+
   def leak
     instance_variable_set("@leak#{rand(100000000)}".to_sym, (File.read(__FILE__)  * @leakiness))
   end
